b4fe5509e92e67d52a2b752fe2494238ba7d8e93
Narek <apres2024@yandex.ru>
2019-12-16 21:13:36 +0300

    fix types and desgn in pricetag

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,26 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-    const modal = document.querySelector('.modal')
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]')     
-    const closseBtn = document.querySelector('.modal__close')
-    const switchModal = () => {
-        modal.classList.toggle('modal--visible')
-    }
-    modalBtn.forEach(element => {
-        element.addEventListener('click', switchModal );
+// document.addEventListener('DOMContentLoaded', function(event) {
+//     const modal = document.querySelector('.modal')
+//     const modalBtn = document.querySelectorAll('[data-toggle=modal]')     
+//     const closseBtn = document.querySelector('.modal__close')
+//     const switchModal = () => {
+//         modal.classList.toggle('modal--visible')
+//     }
+//     modalBtn.forEach(element => {
+//         element.addEventListener('click', switchModal );
 
-    });
+//     });
 
-    closseBtn.addEventListener('click', switchModal );
+//     closseBtn.addEventListener('click', switchModal );
     
+// });
+
+
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closseBtn = $('.modal__close');
+
+        
+    modalBtn.on('click' )
+
 });
