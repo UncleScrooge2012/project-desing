a70d202248a0de7ff99ec74697680b66f118d6dd
Narek <apres2024@yandex.ru>
2019-12-22 18:33:42 +0300

    add scroll-up

--- app/js/main.js
+++ app/js/main.js
@@ -35,6 +35,30 @@ toggleModal = () => {
   toggleModal();
 
 
+  $(document).ready(function(){
+    $('body').append('<a href="#" class="scroll-up">листайте вверх</a>');
+  });
+  
+  $(function() {
+   $.fn.scrollToTop = function() {
+    $(this).hide().removeAttr("href");
+    if ($(window).scrollTop() >= "800") $(this).fadeIn("slow")
+    var scrollDiv = $(this);
+    $(window).scroll(function() {
+     if ($(window).scrollTop() <= "800") $(scrollDiv).fadeOut("slow")
+     else $(scrollDiv).fadeIn("slow")
+    });
+    $(this).click(function() {
+     $("html, body").animate({scrollTop: 0}, "slow")
+    })
+   }
+  });
+  
+  $(function() {
+   $(".scroll-up").scrollToTop();
+  });
+
+
 $(document).ready(function () {
     var modal = $('.modal'),
         modalBtn = $('[data-toggle=modal]'),
